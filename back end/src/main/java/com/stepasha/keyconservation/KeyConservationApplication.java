aaef6ad89b72ff11c2d94876d14422bfe92c48a3
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-01-25 23:37:36 -0700

    back end for key conservation
    starter project

---
+++ back end/src/main/java/com/stepasha/keyconservation/KeyConservationApplication.java
@@ -0,0 +1,13 @@
+package com.stepasha.keyconservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeyConservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeyConservationApplication.class, args);
+    }
+
+}
---
+++ back end/src/test/java/com/stepasha/keyconservation/KeyConservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepasha.keyconservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeyConservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
