3a93c7a048702b00ca143884eba0921a6ab76091
Albert Yakubov <46692454+albert-yakubov@users.noreply.github.com>
2020-01-28 17:50:47 -0700

    bottom navigation complete

--- front end/Key Conservation/app/src/main/java/com/stepasha/keyconservation/MainActivity.kt
+++ front end/Key Conservation/app/src/main/java/com/stepasha/keyconservation/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.stepasha.keyconservation
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.stepasha.keyconservation.adapter.RecyclerViewAdapter
 import com.stepasha.keyconservation.model.Campaign
 import com.stepasha.keyconservation.retrofit.ServiceBuilder
@@ -12,7 +15,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
     companion object{
         var campaign: MutableList<Campaign>? = null
@@ -21,6 +24,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navigation.setOnNavigationItemSelectedListener(this)
 
         getAllCampaigns()
 
@@ -53,5 +57,30 @@ class MainActivity : AppCompatActivity() {
             }
 
         })
+    }
+
+
+    override fun onNavigationItemSelected(p0: MenuItem): Boolean {
+
+
+//change fragment with each menu click
+
+        when (p0.itemId) {
+
+            R.id.navigation_home -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.navigation_map -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+            R.id.navigation_profile -> {
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+            }
+
+        }
+        return true
     }
 }
